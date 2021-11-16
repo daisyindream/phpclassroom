7f296c636487b174fc50045ce6f4b85c609fab4e
daisyindream <72967239+daisyindream@users.noreply.github.com>
2021-11-16 08:54:53 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,18 @@
+<html>
+<head></head>
+<body>
+	<table border="1">
+	<tr>			
+		<td>column 1</td>
+		<td>column 2</td>					
+    <td>column 3</td>
+    <td>column 4</td>
+  </tr>
+  <tr>
+    <td>column 1</td>
+    <td>column 2</td>
+    <td>column 3</td>
+    <td>column 4</td>
+  </tr>
+  </table>
+</html>
